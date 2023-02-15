b155e9af58d4a0554d14dcfd2ca46cb27a040cb0
alexis.herrera <aherrera@teravisiontech.com>
2023-02-15 17:31:55 -0400

    Actulizacion del Repo

--- cypress/e2e/3-testcases/PruebasAvanzadas.cy.js
+++ cypress/e2e/3-testcases/PruebasAvanzadas.cy.js
@@ -1,36 +1,32 @@
 /// <reference types="cypress"/>
 
 
-describe('Suite de casos de pruebas Avazandas ', function () {
-
+describe('Tercer feature de casos avanzados', function () {
     before(function () {
         //Cargamos los valores del archivo example.json en un objeto de datos
-        cy.fixture('carritoDeCompra').then((data) => {
-          this.data = data
+        cy.fixture('carritoDeCompra').then(function (datos) {
+            this.datos = datos
         })
-        cy.log('This: ' + this.data)
+    })
+    beforeEach(() => {
+        //ingresar a la pagina de compra de articulos tecnologicos
+        cy.visit("https://demo.opencart.com/index.php")
     })
 
-    beforeEach(()=>{
-        //Ingresar a la pagina de compra de alticulos
-        cy.visit('https://demo.opencart.com/')
-        cy.log('This: ' + this.data.telefono1)
-
-    })
-
-    it('Compra de celular basado en su id', () => {
+    it('Compra de celular basado en su id', function()  {
         
         cy.get('#menu ul a:contains("Phones & PDAs")').click();
         cy.get('h2').should('have.text','Phones & PDAs');
 
         //div[class='product-thumb']:has(.description) h4 a .button-group > button[data-bs-original-title^="Add to Cart"]
         //div[class='product-thumb']:has(.description):contains('HTC Touch HD') button[data-bs-original-title^="Add to Cart"]
+        cy.log('This: ' + this.data.articulo)
 
-        cy.log(this.data.telefono1)
-        cy.agregarElementoAlCarrito(this.data.telefono1)
-        cy.agregarElementoAlCarrito(this.data.telefono2)
-        cy.agregarElementoAlCarrito(this.data.telefono3)
-
+        this.data.articulo.forEach((articulo) => {
+            cy.agregarElementoAlCarrito(articulo)          
+            cy.log(articulo)
+        });
+        
     });
     
 })
--- cypress/e2e/3-testcases/pruebaFixture.cy.js
+++ cypress/e2e/3-testcases/pruebaFixture.cy.js
@@ -1,19 +1,39 @@
 /// <reference types="cypress"/>
 
 describe('User page', () => {
-    beforeEach(function () {
+    before(function () {
       // "this" points at the test context object
-      cy.fixture('carritoDeCompra').then((user) => {
+      cy.fixture('carritoDeCompra').then((data) => {
         // "this" is still the test context object
-        this.user = user
+        this.data = data
       })
-      cy.log('This: ' + this.user)
+    })
+
+    beforeEach(()=>{
+        //Ingresar a la pagina de compra de alticulos
+        cy.visit('https://demo.opencart.com/')
+        cy.log('This: ' + this.data)
+
     })
   
     // the test callback is in "function () { ... }" form
     it('has user', function () {
       // this.user exists
-      cy.log('This: ' + this.user)
-      expect(this.user.telefono1).to.equal('HTC Touch HD')
+      cy.log('This: ' + this.data) 
     })
+
+    it('Caso numero 2', function () {
+        // this.user exists
+        cy.log('This: ' + this.data)
+
+        cy.get('#menu ul a:contains("Phones & PDAs")').click();
+        cy.get('h2').should('have.text','Phones & PDAs');
+
+        this.data.articulo.forEach((articulo) => {
+            cy.agregarElementoAlCarrito(articulo)          
+            cy.log(articulo)
+        });
+   
+    })
+
   })
